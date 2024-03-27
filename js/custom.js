4d6532751bfd04d7dcff35a4ed95c2a9616b0921
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-03-27 21:50:23 +0300

    added jQuery and other js files

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+import "@/js/loader";
+
+(function ($) {
+  "use strict";
+
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+})(window.jQuery);
